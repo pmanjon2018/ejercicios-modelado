d006ad66473e0d6323bdf41af327ccfe715ea93d
p.manjon.2018 <p.manjon.2018@alumnos.urjc.es>
2026-02-10 02:01:19 +0100

    ejercicio 1 acabado

--- ejercicio1/ejercicio1.py
+++ ejercicio1/ejercicio1.py
@@ -1,3 +1,5 @@
+#python3 -m ejercicio1.py
+
 import pybullet as p
 import pybullet_data
 import time
@@ -16,10 +18,12 @@ startPosition = [0, 0, 1]
 robotId = p.loadURDF("ejercicio1.urdf", startPosition, startOrientation)
 
 # Sliders para controlar los giros
-slider_horizontal = p.addUserDebugParameter("horizontal_z", -math.pi, math.pi, 0.0)
-slider_vertical = p.addUserDebugParameter("vertical_y", -math.pi/2, math.pi/2, 0.0)
+slider_horizontal = p.addUserDebugParameter("Horizontal Z", -math.pi, math.pi, 0.0)
+slider_vertical = p.addUserDebugParameter("Vertical Y", -math.pi/2, math.pi/2, 0.0)
 
 for i in range(10000):
+    p.setJointMotorControl2(robotId, 0, p.POSITION_CONTROL, p.readUserDebugParameter(slider_horizontal))
+    p.setJointMotorControl2(robotId, 1, p.POSITION_CONTROL, p.readUserDebugParameter(slider_vertical))
     p.stepSimulation()
     time.sleep(1./240.)
 
